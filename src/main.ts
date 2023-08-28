027078335048e1fd91f19932b0b842cf764a3aba
mikezhuang <2541413819@qq.com>
2023-08-28 10:16:36 +0800

    feat: 路由

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import VueRouter from 'vue-router'
+import Home from './pages/Home.vue'
+import Project from './pages/Project.vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: Home },
+    { path: '/project', component: Project },
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
